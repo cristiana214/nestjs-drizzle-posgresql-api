c82c7688dd4e1a8b2492823ce67d99bd9c0e8f4b
cristiana214 <cristianachavez214@gmail.com>
2025-02-18 00:58:30 +0800

    🔧 Integrate ConfigModule for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideosModule } from './videos/videos.module';
-
+//https://docs.nestjs.com/techniques/configuration
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [VideosModule],
+  imports: [ConfigModule.forRoot(), VideosModule],
   controllers: [AppController],
   providers: [AppService],
 })
